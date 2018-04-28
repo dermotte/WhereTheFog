492b5d514063664a15dfe36eee49964c42ef6c1e
lukas <l.knoch@gmx.at>
2018-04-28 20:15:41 +0100

    added overflow: hidden to fix scroll bar issue

--- js/main.js
+++ js/main.js
@@ -187,7 +187,7 @@ var animate = function () {
 function onWindowResize(){
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight);
 }
 
 
